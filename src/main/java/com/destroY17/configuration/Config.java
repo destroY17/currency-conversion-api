3b677bafb7d3533c46e68c715fa6bbd1f137b755
destroY17 <eykheandrey@mail.ru>
2023-08-15 20:37:07 +0300

    add transaction management

--- src/main/java/com/destroY17/configuration/Config.java
+++ src/main/java/com/destroY17/configuration/Config.java
@@ -5,7 +5,10 @@ import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,29 +22,39 @@ import java.util.Properties;
 @ComponentScan("com.destroY17")
 @EnableWebMvc
 @EnableTransactionManagement
-public class Config implements WebMvcConfigurer {
+public class Config implements WebMvcConfigurer, TransactionManagementConfigurer {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
     @Bean
-    public DataSource dataSource() throws IOException {
+    public DataSource dataSource() {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
 
-        InputStream inputStream = PGSimpleDataSource.class.getResourceAsStream("/db/db.properties");
-        Properties properties = new Properties();
-        properties.load(inputStream);
+        try {
+            InputStream inputStream = PGSimpleDataSource.class.getResourceAsStream("/db/db.properties");
+            Properties properties = new Properties();
+            properties.load(inputStream);
 
-        dataSource.setURL(properties.getProperty("db.URL"));
-        dataSource.setUser(properties.getProperty("db.USERNAME"));
-        dataSource.setPassword(properties.getProperty("db.PASSWORD"));
+            dataSource.setURL(properties.getProperty("db.URL"));
+            dataSource.setUser(properties.getProperty("db.USERNAME"));
+            dataSource.setPassword(properties.getProperty("db.PASSWORD"));
 
-        return dataSource;
+            return dataSource;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error loading database properties: " + e.getMessage());
+        }
     }
 
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
+    }
+
+    @Override
+    public TransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource());
     }
 }
